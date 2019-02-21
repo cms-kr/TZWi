049fe90bf3bf379d5118aa83af4c46976920fe8b
Junghwan John Goh <jhgoh@cern.ch>
2019-02-22 02:06:41 +0900

    fix typo

--- TopAnalysis/test/prod_ntuple/run.sh
+++ TopAnalysis/test/prod_ntuple/run.sh
@@ -19,6 +19,7 @@ DATATYPE1=`basename $FILELIST | sed -e 's;.txt;;g' | cut -d. -f1`
 DATATYPE2=`basename $FILELIST | sed -e 's;.txt;;g' | cut -d. -f2 | cut -d_ -f2`
 [ _$DATATYPE2 == _ ] && DATATYPE2=$DATATYPE1
 [ _$DATATYPE1 == _RD -a $DATATYPE2 != Run2016H ] && DATATYPE2=Run2016BG
+[ _$DATATYPE1 == _MC ] && DATATYPE2=MC
 
 FILENAMES=$(cat $FILELIST | xargs -n$MAXFILES | sed -n "$(($JOBNUMBER+1)) p" | sed 's;/xrootd/;root://cms-xrdr.sdfarm.kr//xrd/;g')
 
