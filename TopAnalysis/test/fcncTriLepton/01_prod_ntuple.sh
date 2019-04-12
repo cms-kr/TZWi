d18971d782a887a45b2fcdeacd3fe819e7a63a75
jeongsumin <yami4515@naver.com>
2019-04-12 17:25:31 +0900

    typo from case loop (RunB has ver1 and 2)

--- TopAnalysis/test/fcncTriLepton/01_prod_ntuple.sh
+++ TopAnalysis/test/fcncTriLepton/01_prod_ntuple.sh
@@ -34,7 +34,7 @@ ERA4HLT=$DATATYPE
 if [ ${DATATYPE::3} == "Run" ]; then
 # DATATYPE=${DATATYPE0::7} ## This gives Run2018A -> Run2018
   case $ERA in
-    Run2016B|Run2016C|Run2016D|Run2016E)
+    Run2016B_ver1|Run2016B_ver2|Run2016C|Run2016D|Run2016E)
       ERA4HLT=Run2016BE
       ;;
     Run2016F|Run2016G)
