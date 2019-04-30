e25e1355fb96a3e35745ad37c457241ec6012f60
Junghwan John Goh <jhgoh@cern.ch>
2019-05-01 04:22:38 +0900

    load correct configuration

--- TopAnalysis/python/postprocessing/fcnhSingleLeptonHLT.py
+++ TopAnalysis/python/postprocessing/fcnhSingleLeptonHLT.py
@@ -3,8 +3,8 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 from TZWi.TopAnalysis.postprocessing.CombineHLT import CombineHLT
 
-hlt_MC2017_Mu = lambda : CombineHLT(fileName="fcncTriLepton/2017.yaml", hltSet="RunIIFall17.SingleMuon")
-hlt_MC2017_El = lambda : CombineHLT(fileName="fcncTriLepton/2017.yaml", hltSet="RunIIFall17.SingleElectron")
+hlt_MC2017_Mu = lambda : CombineHLT(fileName="fcnhSingleLepton/2017.yaml", hltSet="RunIIFall17.SingleMuon")
+hlt_MC2017_El = lambda : CombineHLT(fileName="fcnhSingleLepton/2017.yaml", hltSet="RunIIFall17.SingleElectron")
 
-hlt_Run2017_Mu = lambda : CombineHLT(fileName="fcncTriLepton/2017.yaml", hltSet="Run2017.SingleMuon"    , doFilter=True)
-hlt_Run2017_El = lambda : CombineHLT(fileName="fcncTriLepton/2017.yaml", hltSet="Run2017.SingleElectron", doFilter=True)
+hlt_Run2017_Mu = lambda : CombineHLT(fileName="fcnhSingleLepton/2017.yaml", hltSet="Run2017.SingleMuon"    , doFilter=True)
+hlt_Run2017_El = lambda : CombineHLT(fileName="fcnhSingleLepton/2017.yaml", hltSet="Run2017.SingleElectron", doFilter=True)
