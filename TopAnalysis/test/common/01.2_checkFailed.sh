29b2c0c98f7bb6f8948d8c97492d8febe6b0e776
Junghwan John Goh <jhgoh@cern.ch>
2019-05-18 05:56:33 +0900

    helper scripts to submit and babysit jobs

---
+++ TopAnalysis/test/common/01.2_checkFailed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for i in submit/*NANOAOD/ submit/*NANOAODSIM/; do
+    cd $i
+    [ -f failed.txt ] && rm -f failed.txt
+    for j in result*.tgz; do
+        tar tzf $j | grep -q failed.txt && tar -Oxzf $j failed.txt >> failed.txt 2> /dev/null
+    done
+    cd ../..
+done
+
+for i in submit/*NANOAOD/ submit/*NANOAODSIM/; do
+    cd $i
+    if [ -f failed.txt ]; then
+        rm -f result*.tgz job*.err job*.log failed.txt
+        ./submit.sh
+    fi
+    cd ../..
+done
