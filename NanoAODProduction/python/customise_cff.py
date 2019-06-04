21488cd902ee0c2ce24cfe39710a347b0fc4440b
YongHoon Lee <sekikai@naver.com>
2019-06-04 16:10:20 +0900

    gentop production

--- NanoAODProduction/python/customise_cff.py
+++ NanoAODProduction/python/customise_cff.py
@@ -3,3 +3,4 @@
 from TZWi.NanoAODProduction.customise_dropNanoAODTables_cff import *
 from TZWi.NanoAODProduction.customise_particleTop_cff import *
 from TZWi.NanoAODProduction.customise_partonTop_cff import *
+from TZWi.NanoAODProduction.customise_genTop_cff import *
