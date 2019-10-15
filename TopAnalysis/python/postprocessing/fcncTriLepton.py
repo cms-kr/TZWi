d0aa2aea69d01816f30b7fd748dd284bbd0663c2
jeongsumin <yami4515@naver.com>
2019-10-15 15:53:15 +0900

    changing and adding some variable names, adding NPL lambda functions

--- TopAnalysis/python/postprocessing/fcncTriLepton.py
+++ TopAnalysis/python/postprocessing/fcncTriLepton.py
@@ -37,10 +37,11 @@ class FCNCTriLepton(Module, object):
         self.out.branch("Lepton1_pdgId", "I")
         self.out.branch("Lepton2_pdgId", "I")
         self.out.branch("Lepton3_pdgId", "I")
-        self.out.branch("LeptonTotal_mass", "F")
-        self.out.branch("LeptonTotal_pt", "F")
-        self.out.branch("LeptonWandZ_deltaPhi", "F")
-        self.out.branch("LeptonWandZ_deltaR", "F")
+	self.out.branch("LeadingLepton_pt", "F")
+        self.out.branch("TriLepton_mass", "F")
+        self.out.branch("TriLepton_pt", "F")
+        self.out.branch("TriLepton_WleptonZdPhi", "F")
+        self.out.branch("TriLepton_WleptonZdR", "F")
         self.out.branch("Z_charge", "I")
         self.out.branch("nVetoLepton", "i")
         self.out.branch("GoodLeptonCode", "I")
@@ -69,7 +70,7 @@ class FCNCTriLepton(Module, object):
         objName = "Muon"
         setattr(self, "b_n%s" % objName, tree.valueReader("n%s" % objName))
         for varName in ["pt", "eta", "phi", "mass", "charge",
-                        "pfRelIso04_all", "tightId", "isGlobal", "isPFcand", "isTracker"]:
+                        "pfRelIso04_all", "looseId", "tightId", "isGlobal", "isPFcand", "isTracker"]:
             setattr(self, "b_%s_%s" % (objName, varName), tree.arrayReader("%s_%s" % (objName, varName)))
 
         objName = "Jet"
@@ -83,7 +84,7 @@ class FCNCTriLepton(Module, object):
                                  self.b_Electron_pfRelIso03_all, getattr(self, 'b_Electron_%s' % self.eleIdName),
                                  self.b_Electron_deltaEtaSC, self.b_Electron_eCorr)
         self.worker.setMuons(self.b_Muon_pt, self.b_Muon_eta, self.b_Muon_phi, self.b_Muon_mass, self.b_Muon_charge,
-                             self.b_Muon_pfRelIso04_all,self.b_Muon_tightId, self.b_Muon_isGlobal, self.b_Muon_isPFcand, self.b_Muon_isTracker)
+                             self.b_Muon_pfRelIso04_all, self.b_Muon_looseId, self.b_Muon_tightId, self.b_Muon_isGlobal, self.b_Muon_isPFcand, self.b_Muon_isTracker)
         self.worker.setJets(self.b_Jet_pt, self.b_Jet_eta, self.b_Jet_phi, self.b_Jet_mass,
                             self.b_Jet_jetId, self.b_Jet_btagCSVV2)
         self._ttreereaderversion = tree._ttreereaderversion
@@ -99,8 +100,8 @@ class FCNCTriLepton(Module, object):
             for varName in ["pt", "eta", "phi", "mass"]:
                 setattr(event._tree, "b_out_%s_%s" % (objName, varName), getattr(self.worker, 'get_%s_%s' % (objName, varName))())
                 self.out.fillBranch("%s_%s" % (objName, varName), getattr(event._tree, 'b_out_%s_%s' % (objName, varName)))
-        for varName in ["MET_pt", "MET_phi", "Lepton1_pdgId", "Lepton2_pdgId", "Lepton3_pdgId",
-                        "LeptonTotal_mass", "LeptonTotal_pt", "LeptonWandZ_deltaPhi", "LeptonWandZ_deltaR",
+        for varName in ["MET_pt", "MET_phi", "Lepton1_pdgId", "Lepton2_pdgId", "Lepton3_pdgId", "LeadingLepton_pt",
+                        "TriLepton_mass", "TriLepton_pt", "TriLepton_WleptonZdPhi", "TriLepton_WleptonZdR",
                         "nVetoLepton", "GoodLeptonCode", "Z_charge", "W_MT",
                         #"nGoodJet", #We do not keep nGoodJet here, it have to be done by the framework
                         "GoodJet_index", "GoodJet_CSVv2",
@@ -116,8 +117,16 @@ fcnc_MuMuMu_2016 = lambda : FCNCTriLepton(mode="MuMuMu", eleId="cutBased_Sum16")
 fcnc_ElElEl_2016 = lambda : FCNCTriLepton(mode="ElElEl", eleId="cutBased_Sum16")
 fcnc_ElMuMu_2016 = lambda : FCNCTriLepton(mode="ElMuMu", eleId="cutBased_Sum16")
 fcnc_MuElEl_2016 = lambda : FCNCTriLepton(mode="MuElEl", eleId="cutBased_Sum16")
+fcnc_NPLMuMuMu_2016 = lambda : FCNCTriLepton(mode="NPLMuMuMu", eleId="cutBased_Sum16")
+fcnc_NPLElElEl_2016 = lambda : FCNCTriLepton(mode="NPLElElEl", eleId="cutBased_Sum16")
+fcnc_NPLElMuMu_2016 = lambda : FCNCTriLepton(mode="NPLElMuMu", eleId="cutBased_Sum16")
+fcnc_NPLMuElEl_2016 = lambda : FCNCTriLepton(mode="NPLMuElEl", eleId="cutBased_Sum16")
 
 fcnc_MuMuMu_2017 = lambda : FCNCTriLepton(mode="MuMuMu", eleId="cutBased_Fall17_V1")
 fcnc_ElElEl_2017 = lambda : FCNCTriLepton(mode="ElElEl", eleId="cutBased_Fall17_V1")
 fcnc_ElMuMu_2017 = lambda : FCNCTriLepton(mode="ElMuMu", eleId="cutBased_Fall17_V1")
 fcnc_MuElEl_2017 = lambda : FCNCTriLepton(mode="MuElEl", eleId="cutBased_Fall17_V1")
+fcnc_NPLMuMuMu_2017 = lambda : FCNCTriLepton(mode="NPLMuMuMu", eleId="cutBased_Fall17_V1")
+fcnc_NPLElElEl_2017 = lambda : FCNCTriLepton(mode="NPLElElEl", eleId="cutBased_Fall17_V1")
+fcnc_NPLElMuMu_2017 = lambda : FCNCTriLepton(mode="NPLElMuMu", eleId="cutBased_Fall17_V1")
+fcnc_NPLMuElEl_2017 = lambda : FCNCTriLepton(mode="NPLMuElEl", eleId="cutBased_Fall17_V1")
