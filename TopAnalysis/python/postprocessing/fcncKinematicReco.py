396e624f71bf164cb94ade2b1dbf57067f6f78cd
YongHoon Lee <sekikai@naver.com>
2020-01-28 10:15:01 +0900

    Add missed items

---
+++ TopAnalysis/python/postprocessing/fcncKinematicReco.py
@@ -0,0 +1,181 @@
+import ROOT
+import math
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+import os
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+
+class FCNCKinematicReco(Module, object):
+    def __init__(self, *args, **kwargs):
+        pass
+    def beginJob(self):
+        pass
+    def endJob(self):
+        pass
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        self.out = wrappedOutputTree
+        self.out.branch("SMTop_pt", "F")
+        self.out.branch("SMTop_eta", "F")
+        self.out.branch("SMTop_phi", "F")
+        self.out.branch("SMTop_mass", "F")
+        self.out.branch("FCNCTop_pt", "F")
+        self.out.branch("FCNCTop_eta", "F")
+        self.out.branch("FCNCTop_phi", "F")
+        self.out.branch("FCNCTop_mass", "F")
+        self.out.branch("Reconstructed", "i")
+
+        pass
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+    def getKinVar(self, pt, eta, phi, mass):
+        KinVar = []
+        px = pt*(math.cos(phi))
+        py = pt*(math.sin(phi))
+        pz = pt*(math.sinh(eta))
+        epart = math.sqrt(px*px+py*py+pz*pz+mass*mass) ##
+        KinVar = [px, py, pz, epart]
+        return KinVar
+
+    def getSol(self, plx, ply, plz, El, lmass, pnx, pny, Wmass):
+        possolu = 0
+        negsolu = 0
+        constflag = 1
+        A = 2*(plx*pnx+ply*pny)+(Wmass*Wmass)-(lmass*lmass)
+        B = (4*(El*El)*(pnx*pnx+pny*pny))-(A*A)
+        aterm = A*plz
+        bterm = A*A*plz*plz
+        cterm = El*El-plz*plz
+        if ( bterm - B*cterm > 0 ):
+            possolu = (aterm + math.sqrt(bterm-B*cterm))/(2*cterm)
+            negsolu = (aterm - math.sqrt(bterm-B*cterm))/(2*cterm)
+            constflag = 1
+        else:
+            possolu = 0
+            negsolu = 0
+            constflag = 0
+        solu = [possolu, negsolu, constflag]
+        #solu = [possolu, negsolu, constflag, A, B, aterm, bterm, cterm] # For debugging
+        return solu
+    def getTPEPM(self, pxs, pys, pzs, Es):
+        sumpx = 0
+        sumpy = 0
+        sumpz = 0
+        sumE = 0
+        for i, valx in enumerate(pxs):
+            sumpx += valx
+        for i, valy in enumerate(pys):
+            sumpy += valy
+        for i, valz in enumerate(pzs):
+            sumpz += valz
+        for i, valE in enumerate(Es):
+            sumE += valE
+        TPt = math.sqrt(sumpx*sumpx+sumpy*sumpy)
+        TPhi = math.atan2(sumpy,sumpx) # atan2(y,x)
+        TEta = math.asinh(sumpz/TPt)
+        TMass = math.sqrt((sumE*sumE)-(sumpx*sumpx)-(sumpy*sumpy)-(sumpz*sumpz))
+        TKinVar = [TPt, TEta, TPhi, TMass]
+        #TKinVar = [TPt, TEta, TPhi, TMass, sumpx, sumpy, sumpz, sumE] # For debugging
+        return TKinVar
+
+    def analyze(self, event):
+        Wmass = 80.4 # Fixed W mass
+        OriginTmass = 172.5 # Fixed T mass
+
+        ## initialize ##
+        SMTKinVal = []
+        FCNCKinVal = []
+
+        if ( event._tree.b_out_GoodLeptonCode != 111 ): return False
+        if ( event._tree.b_out_nGoodLepton > 3 ): return False
+        if not ( 2 <= event._tree.b_out_nGoodJet <= 3 ): return False
+        #if ( event._tree.b_out_nBjet < 1): break
+        # Variable construct : Lepton vars = [pt, eta, phi, mass]
+        Zlep1var = [event._tree.b_out_Lepton2_pt, event._tree.b_out_Lepton2_eta, event._tree.b_out_Lepton2_phi, event._tree.b_out_Lepton2_mass]
+        Zlep2var = [event._tree.b_out_Lepton3_pt, event._tree.b_out_Lepton3_eta, event._tree.b_out_Lepton3_phi, event._tree.b_out_Lepton3_mass]
+        Wlepvar = [event._tree.b_out_Lepton1_pt, event._tree.b_out_Lepton1_eta, event._tree.b_out_Lepton1_phi, event._tree.b_out_Lepton1_mass]
+        # Variable coonstruct : Jet vars = [pt, eta, phi, mass, CSVv2]
+        #if event._tree._b_out_nGoodJet < 2: continue # nJet >= 2 for tZq reconstruction
+        jet1var = [event._tree.b_out_GoodJet_pt[0], event._tree.b_out_GoodJet_eta[0], event._tree.b_out_GoodJet_phi[0], event._tree.b_out_GoodJet_mass[0], event._tree.b_out_GoodJet_CSVv2[0]]
+        jet2var = [event._tree.b_out_GoodJet_pt[1], event._tree.b_out_GoodJet_eta[1], event._tree.b_out_GoodJet_phi[1], event._tree.b_out_GoodJet_mass[1], event._tree.b_out_GoodJet_CSVv2[1]]
+        # Variable condtruct : Neutrino vars = [MET, phi]
+        metvar = [event._tree.b_out_MET_pt, event._tree.b_out_MET_phi]
+
+        # b jet assign by CSVv2 discriminator
+        if ( jet1var[4] > jet2var[4] ): 
+            bvar = jet1var
+            qvar = jet2var
+        else:
+            bvar = jet2var
+            qvar = jet1var
+
+        # pxyzE calculation & construction : [px, py, pz, E]
+        Zlep1 = self.getKinVar(Zlep1var[0], Zlep1var[1], Zlep1var[2], 0.)
+        Zlep2 = self.getKinVar(Zlep2var[0], Zlep2var[1], Zlep2var[2], 0.)
+        Wlep = self.getKinVar(Wlepvar[0], Wlepvar[1], Wlepvar[2], 0.) ## Assume lepton mass = 0
+        bjet = self.getKinVar(bvar[0], bvar[1], bvar[2], bvar[3])
+        qjet = self.getKinVar(qvar[0], qvar[1], qvar[2], qvar[3])
+        
+        # MET pxy calculation & construction : [px, py]
+        metpx = metvar[0]*(math.cos(metvar[1]))
+        metpy = metvar[0]*(math.sin(metvar[1]))
+        met = [metpx, metpy]
+
+        # Calculate pz_neu and E_neu
+        metpz = self.getSol(Wlep[0], Wlep[1], Wlep[2], Wlep[3], 0., met[0], met[1], Wmass) # Assume lepton mass = 0
+        posneuE = math.sqrt((metpx*metpx)+(metpy*metpy)+(metpz[0]*metpz[0]))
+        negneuE = math.sqrt((metpx*metpx)+(metpy*metpy)+(metpz[1]*metpz[1]))
+        consted = metpz[2] # Reconstructed flag
+
+        # SM Top mass construction
+        SMpxs = [bjet[0], Wlep[0], met[0]]
+        SMpys = [bjet[1], Wlep[1], met[1]]
+        posSMpzs = [bjet[2], Wlep[2], metpz[0]]
+        negSMpzs = [bjet[2], Wlep[2], metpz[1]]
+        posSMEs= [bjet[3], Wlep[3], posneuE]
+        negSMEs= [bjet[3], Wlep[3], negneuE]
+
+        posTMass = self.getTPEPM(SMpxs, SMpys, posSMpzs, posSMEs)[3]
+        negTMass = self.getTPEPM(SMpxs, SMpys, negSMpzs, negSMEs)[3]
+        
+        ## Top mass variance comparison and reconsruct SM Top KinVals
+        if ( math.fabs(posTMass - OriginTmass) < math.fabs(negTMass - OriginTmass) ):
+            SMTMass = posTMass
+            SMTKinVal = self.getTPEPM(SMpxs, SMpys, posSMpzs, posSMEs)
+        else:
+            SMTMass = negTMass
+            SMTKinVal = self.getTPEPM(SMpxs, SMpys, negSMpzs, negSMEs)
+
+        # FCNC Top reconstruction
+        FCNCpxs = [qjet[0], Zlep1[0], Zlep2[0]]
+        FCNCpys = [qjet[1], Zlep1[1], Zlep2[1]]
+        FCNCpzs = [qjet[2], Zlep1[2], Zlep2[2]]
+        FCNCEs = [qjet[3], Zlep1[3], Zlep2[3]]
+        FCNCTKinVal = self.getTPEPM(FCNCpxs, FCNCpys, FCNCpzs, FCNCEs)
+
+        self.out.fillBranch("Reconstructed", consted)
+        if ( consted == 0 ): return False
+        self.out.fillBranch("SMTop_pt", SMTKinVal[0])
+        self.out.fillBranch("SMTop_eta", SMTKinVal[1])
+        self.out.fillBranch("SMTop_phi", SMTKinVal[2])
+        self.out.fillBranch("SMTop_mass", SMTKinVal[3])
+        self.out.fillBranch("FCNCTop_pt", FCNCTKinVal[0])
+        self.out.fillBranch("FCNCTop_eta", FCNCTKinVal[1])
+        self.out.fillBranch("FCNCTop_phi", FCNCTKinVal[2])
+        self.out.fillBranch("FCNCTop_mass", FCNCTKinVal[3])
+
+        ## for debugging
+        #print " Coefficients A/B/aterm/bterm/cterm : ", metpz[3], metpz[4], metpz[5], metpz[6], metpz[7]
+        #print " Sum pxyzE for top solution : ", SMTKinVal[4], SMTKinVal[5], SMTKinVal[6], SMTKinVal[7]
+        #print "Z lepton 1 : ", Zlep1
+        #print "Z lepton 2 : ", Zlep2
+        #print "W lepton 1 : ", Wlep
+        #print "b jet : ", bjet
+        #print "q jet : ", qjet
+        #print "neu px, py : ", met 
+        #print " Neu Z pos/neg solution : ", metpz[0], metpz[1]
+        #print "neu pos/neg energy : ", posneuE, negneuE
+
+        return True
+
+fcncKinReco = lambda: FCNCKinematicReco()
---
+++ TopAnalysis/test/fcncTriLepton/bkgestFit_working.py
@@ -0,0 +1,232 @@
+from ROOT import *
+import sys, json, os, math
+#from sysWeight_cfi import *
+from array import array
+
+gROOT.SetBatch(True)
+def addLegendCMS():
+  #tex2 = TLatex(0.3715952,0.9146667,"Preliminary")
+  tex2 = TLatex(-20.,50.,"Preliminary")
+  tex2.SetNDC()
+  tex2.SetTextAlign(12)
+  tex2.SetX(0.25)
+  tex2.SetY(0.97)
+  tex2.SetTextColor(2)
+  tex2.SetTextFont(42)
+  tex2.SetTextSize(0.05)
+  tex2.SetTextSizePixels(24)
+  #tex2.Draw()
+
+  return tex2
+
+def make_legend(xmin,ymin,xmax,ymax):
+  #leg = TLegend(0.65,0.7, 0.89,0.89)
+  leg = TLegend(xmin,ymin,xmax,ymax)
+  leg.SetFillColor(0)
+  leg.SetLineColor(1)
+  leg.SetTextFont(62)
+  leg.SetTextSize(0.03)
+
+  leg.SetBorderSize(1)
+  leg.SetLineStyle(1)
+  leg.SetLineWidth(1)
+  leg.SetLineColor(0)
+
+  return leg
+
+def calcError(hist):
+  herr = 0.
+  h = hist
+  h.Sumw2()
+  for i in range(0,30):
+    herr += h.GetBinError(i)
+  return herr
+
+name = "bkgEst"
+result = "fitresult_%s.txt" % name
+baseDir = "./hist_bkgest"
+chlist = ["ElElEl", "MuElEl", "ElMuMu", "MuMuMu"]
+#chlist = ["MuMuMu"]
+mclist = ["WZ", "ZZ", "DYJets"]
+#rootFile = "%s/ElElEl.root" % baseDir
+if os.path.exists(result): os.system("rm -f %s" % result)
+
+for i, ch in enumerate(chlist):
+
+  rootFile = "%s/%s.root" % (baseDir, ch)
+  f = TFile.Open(rootFile)
+
+  # Book hists for WZCR, TTCR
+  hWZ = f.Get("WZCR/hW_MT/WZ")
+  hZZ = f.Get("WZCR/hW_MT/ZZ")
+  hDYJets = f.Get("WZCR/hW_MT/DYJets")
+  hrd = f.Get("WZCR/hW_MT/Data")
+  hTT_all = f.Get("TTCR_allZ/hZ_mass/ttJets")
+  hTTrd = f.Get("TTCR_allZ/hZ_mass/Data")
+  hTTSR = f.Get("TTSR/hZ_mass/ttJets")
+
+  hlist = [hWZ, hZZ, hDYJets]
+
+  hnlist = []
+  for i,h in enumerate(hlist):
+    hnlist.append(h.Integral())
+
+  nWZJets = hnlist[0]
+  nZZs = hnlist[1]
+  nDYJets = hnlist[2]
+
+  totMC = nWZJets+nZZs+nDYJets
+  rWZ = nWZJets/totMC
+  rZZ = nZZs/totMC
+  rDY = nDYJets/totMC
+
+  print "initial RWZ = ", rWZ
+  print "initial RZZ = ", rZZ
+  print "initial RDY = ", rDY
+
+  x = RooRealVar("x", "x", 0, 300)
+
+  fWZ = RooRealVar("fWZ", "fWZ", rWZ, 0.0, 0.9)
+  fZZ = RooRealVar("fZZ", "fZZ", rZZ, 0.0, 0.9)
+  fDY = RooRealVar("fDY", "fDY", rDY, 0.0, 0.9)
+  nWZ = RooRealVar("nWZ", "nWZ", nWZJets, nWZJets, nWZJets)
+  nZZ = RooRealVar("nZZ", "nZZ", nZZs, nZZs, nZZs)
+  nDY = RooRealVar("nDY", "nWZ", nDYJets, nDYJets, nDYJets)
+  ntot = RooRealVar("ntot", "ntot", totMC, totMC, totMC)
+  k = RooRealVar("k", "Normalization Factor", 1, 0.7, 1.3)
+  ktot = RooFormulaVar("ktot", "number of tot event after fitting", "k*ntot", RooArgList(k, ntot))
+
+  xArg = RooArgList(x)
+  data = RooDataHist("data", "data point with x", xArg, hrd)
+  WZ = RooDataHist("WZ", "WZ point with x", xArg, hWZ)
+  ZZ = RooDataHist("ZZ", "ZZ point with x", xArg, hZZ)
+  DYJets = RooDataHist("DYJets", "DY point with x", xArg, hDYJets)
+  WZpdf = RooHistPdf("WZpdf", "WZpdf", RooArgSet(RooArgList(x)), WZ)
+  ZZpdf = RooHistPdf("ZZpdf", "ZZpdf", RooArgSet(RooArgList(x)), ZZ)
+  DYpdf = RooHistPdf("DYpdf", "DYpdf", RooArgSet(RooArgList(x)), DYJets)
+
+  model = RooAddPdf("model", "model", RooArgList(ZZpdf, DYpdf, WZpdf), RooArgList(fZZ, fDY)) # WZ  
+  #model = RooAddPdf("model", "model", RooArgList(WZpdf, DYpdf, ZZpdf), RooArgList(fWZ, fDY)) # ZZ
+  #model = RooAddPdf("model", "model", RooArgList(ZZpdf, WZpdf, DYpdf), RooArgList(fZZ, fWZ)) # DY
+
+  model2 = RooAddPdf("model2", "model2", RooArgList(model), RooArgList(ktot))
+  model2.fitTo(data)
+  k.Print()
+  #fWZ.Print()
+  fZZ.Print()
+  fDY.Print()
+
+  fracZZ = fZZ.getVal()
+  fracDY = fDY.getVal()
+  fracWZ = 1-fracZZ-fracDY
+  fitk = k.getVal()
+
+  fraclist = [fracWZ, fracZZ, fracDY]
+
+  cplot = TCanvas("cplot", "cplot", 1)
+  xframe = x.frame()
+  data.plotOn(xframe)
+  model.plotOn(xframe)
+  #WZ.plotOn(xframe, RooFit.LineColor(2), RooFit.LineStyle(9), RooFit.Rescale(fracWZ))
+  #DYJets.plotOn(xframe, RooFit.LineColor(3), RooFit.LineStyle(9), RooFit.Rescale(fracWZ))
+  xframe.Draw()
+  cplot.SaveAs("fitresult_WZCR_%s.png"%ch)
+
+  #entriesWZ = WZpdf.getAnalyticalIntegral(RooArgSet(xArg), RooArgSet(xArg))
+  #entriesZZ = ZZpdf.getAnalyticalIntegral(RooArgSet(xArg), RooArgSet(xArg))
+  #entriesDY = DYpdf.getAnalyticalIntegral(RooArgSet(xArg), RooArgSet(xArg))
+
+  #print "WZ after fit is = ", 36500*entriesWZ
+  #print "ZZ after fit is = ", 36500*entriesZZ
+  #print "DY after fit is = ", 36500*entriesDY
+
+# Ratio calculation
+  ratio0b = []
+  ratio23j = []
+  estedlist = []
+  ### For temporal calculation ## Should be changed!!
+  nrd = hrd.Integral()
+  ###
+
+### For TTCR
+  y = RooRealVar("y", "y", 30, 150)
+  coef1 = RooRealVar("a", "a", -0.1, 0.1)
+  
+  yArg = RooArgList(y)
+  k1 = RooRealVar("k1", "k1", 0, -0.1, 0.1)
+  TTdata = RooDataHist("TTdata", "Data point with x", yArg, hTTrd)
+  TT = RooDataHist("TT", "TT point with x", yArg, hTT_all)
+  linmodel = RooPolynomial("linmodel", "linmodel", y, RooArgList(coef1)) # Model for linear check
+
+  ## Save Linear check result
+  linmodel.fitTo(TT)
+  coef1.Print()
+  x1 = coef1.getVal()
+  file = open(result, "a")
+  lline = "Linear check TTCR %s %f\n" %(ch, x1)
+  file.write(lline)
+  file.close()
+
+  tcplot = TCanvas("tcplot", "tcplot", 1)
+  yframe = y.frame()
+  TT.plotOn(yframe)
+  linmodel.plotOn(yframe)
+  yframe.Draw()
+  tcplot.SaveAs("LinearCheck_TTCR_%s.png"%ch)
+
+  ## Save TTCR fit Resutl
+
+  nttsr = hTTSR.Integral()
+  errttsr = calcError(hTTSR)
+
+  linmodel.fitTo(TTdata, RooFit.Range(30, 70))
+  coef1.Print()
+  ttplot = TCanvas("ttplot", "ttplot", 1)
+  tframe = y.frame()
+  TTdata.plotOn(tframe)
+  linmodel.plotOn(tframe)
+  #tframe.Draw()
+  t1 = coef1.getVal()
+  linmodel.fitTo(TTdata, RooFit.Range(110, 150))
+  coef1.Print()
+  linmodel.plotOn(tframe)
+  tframe.Draw()
+  t2 = coef1.getVal()
+  ttplot.SaveAs("fitresult_TTCR_%s.png"%ch)
+  t11 = hTT_all.Integral(30, 70)
+  t22 = hTT_all.Integral(110, 150) 
+  file = open(result, "a")
+  tline = "%s TT %f %f\n"%(ch, 15*(abs(t1)+abs(t2)), 35900*nttsr)
+  #tline = "%s TT %f %f\n"%(ch, 36500*(t11+t22)*15/80, 36500*nttsr)
+  file.write(tline)
+  file.close()
+
+### To extract expected entries
+  hexpWZ = f.Get("TTSR/hW_MT/WZ")
+  hexpZZ = f.Get("TTSR/hW_MT/ZZ")
+  hexpDY = f.Get("TTSR/hW_MT/DYJets")
+  hexplist = [hexpWZ, hexpZZ, hexpDY]
+
+  nexplist = []
+  for i,h in enumerate(hexplist):
+    nexplist.append(h.Integral())
+    
+  for i, mc in enumerate(mclist):
+    hista = f.Get("WZCR_0b/hnGoodJet/%s" % mc)    
+    histb = f.Get("WZCR_23jet/hnBjet/%s" % mc)
+    n23j0b = hista.Integral(3,4)
+    n1j0b = hista.Integral()
+    ratio0b.append(n23j0b/n1j0b)
+    n1b23j = histb.Integral(2,2)
+    n0b23j = histb.Integral(1,1)
+    ratio23j.append(n1b23j/n0b23j)
+    ### Should be changed!!! ###
+    estedWZCR = 35900 * totMC * fitk * fraclist[i] * ratio0b[i] * ratio23j[i]
+    ###
+
+    file = open(result, "a")
+    wline = "%s %s %f %f %f %f %f %f\n" %(ch, mc, hnlist[i]*35900, 35900*totMC*fitk*fraclist[i], ratio0b[i], ratio23j[i], estedWZCR, nexplist[i]*35900)
+    file.write(wline)
+    file.close()
+
+  f.Close()
