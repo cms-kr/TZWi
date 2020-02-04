ef312334e8528640dce0c7c57ab046c04cd06996
yhlee52 <sekikai@naver.com>
2020-02-04 20:53:59 +0900

    Validate top reconstruction for all data/MC

--- TopAnalysis/python/postprocessing/fcncKinematicReco.py
+++ TopAnalysis/python/postprocessing/fcncKinematicReco.py
@@ -84,88 +84,103 @@ class FCNCKinematicReco(Module, object):
 
         ## initialize ##
         SMTKinVal = [0,0,0,0]
-        FCNCKinVal = [0,0,0,0]
+        FCNCTKinVal = [0,0,0,0]
         consted = 0
 
-        while True:
-            if ( event._tree.b_out_GoodLeptonCode != 111 ): break
-            if ( event._tree.b_out_nGoodLepton > 3 ): break
-            if not ( 2 <= event._tree.b_out_nGoodJet <= 3 ): break
-            if ( event._tree.b_out_nBjet < 1): break
-            break
+        # Check basic event selection
+        if event._tree.b_out_GoodLeptonCode != 111 or\
+           event._tree.b_out_nGoodLepton > 3 or\
+           not( 2 <= event._tree.b_out_nGoodJet <= 3 ) or\
+           event._tree.b_out_nBjet < 1:
 
-        # Variable construct : Lepton vars = [pt, eta, phi, mass]
-        Zlep1var = [event._tree.b_out_Lepton2_pt, event._tree.b_out_Lepton2_eta, event._tree.b_out_Lepton2_phi, event._tree.b_out_Lepton2_mass]
-        Zlep2var = [event._tree.b_out_Lepton3_pt, event._tree.b_out_Lepton3_eta, event._tree.b_out_Lepton3_phi, event._tree.b_out_Lepton3_mass]
-        Wlepvar = [event._tree.b_out_Lepton1_pt, event._tree.b_out_Lepton1_eta, event._tree.b_out_Lepton1_phi, event._tree.b_out_Lepton1_mass]
-        # Variable coonstruct : Jet vars = [pt, eta, phi, mass, CSVv2]
-        #if event._tree._b_out_nGoodJet < 2: continue # nJet >= 2 for tZq reconstruction
-        jet1var = [event._tree.b_out_GoodJet_pt[0], event._tree.b_out_GoodJet_eta[0], event._tree.b_out_GoodJet_phi[0], event._tree.b_out_GoodJet_mass[0], event._tree.b_out_GoodJet_CSVv2[0]]
-        jet2var = [event._tree.b_out_GoodJet_pt[1], event._tree.b_out_GoodJet_eta[1], event._tree.b_out_GoodJet_phi[1], event._tree.b_out_GoodJet_mass[1], event._tree.b_out_GoodJet_CSVv2[1]]
-        # Variable condtruct : Neutrino vars = [MET, phi]
-        metvar = [event._tree.b_out_MET_pt, event._tree.b_out_MET_phi]
+            self.out.fillBranch("KinTop_status", 0)
+            self.out.fillBranch("KinTopWb_pt", 0)
+            self.out.fillBranch("KinTopWb_eta", 0)
+            self.out.fillBranch("KinTopWb_phi", 0)
+            self.out.fillBranch("KinTopWb_mass", 0)
+            self.out.fillBranch("KinTopZq_pt", 0)
+            self.out.fillBranch("KinTopZq_eta", 0)
+            self.out.fillBranch("KinTopZq_phi", 0)
+            self.out.fillBranch("KinTopZq_mass", 0)
 
-        # b jet assign by CSVv2 discriminator
-        if ( jet1var[4] > jet2var[4] ): 
-            bvar = jet1var
-            qvar = jet2var
+            return True
         else:
-            bvar = jet2var
-            qvar = jet1var
+            # Variable construct : Lepton vars = [pt, eta, phi, mass]
+            Wlepvar = TLorentzVector()
+            Zlep1var = TLorentzVector()
+            Zlep2var = TLorentzVector()
+            Wlepvar.SetPtEtaPhiM(event._tree.b_out_Lepton1_pt, event._tree.b_out_Lepton1_eta, event._tree.b_out_Lepton1_phi, event._tree.b_out_Lepton1_mass)
+            Zlep1var.SetPtEtaPhiM(event._tree.b_out_Lepton2_pt, event._tree.b_out_Lepton2_eta, event._tree.b_out_Lepton2_phi, event._tree.b_out_Lepton2_mass)
+            Zlep2var.SetPtEtaPhiM(event._tree.b_out_Lepton3_pt, event._tree.b_out_Lepton3_eta, event._tree.b_out_Lepton3_phi, event._tree.b_out_Lepton3_mass)
+            # Variable coonstruct : Jet vars = [pt, eta, phi, mass, CSVv2]
+            #if event._tree._b_out_nGoodJet < 2: continue # nJet >= 2 for tZq reconstruction
+            bvar = TLorentzVector()
+            qvar = TLorentzVector()
+            bvar.SetPtEtaPhiM(event._tree.b_out_GoodJet_pt[0], event._tree.b_out_GoodJet_eta[0], event._tree.b_out_GoodJet_phi[0], event._tree.b_out_GoodJet_mass[0])
+            qvar.SetPtEtaPhiM(event._tree.b_out_GoodJet_pt[1], event._tree.b_out_GoodJet_eta[1], event._tree.b_out_GoodJet_phi[1], event._tree.b_out_GoodJet_mass[1])
+            bjetCSV, qjetCSV = event._tree.b_out_GoodJet_CSVv2[0], event._tree.b_out_GoodJet_CSVv2[1]
+            # Variable condtruct : Neutrino vars = [MET, phi]
+            metvar = TLorentzVector()
+            metvar.SetPtEtaPhiM(event._tree.b_out_MET_pt, 0, event._tree.b_out_MET_phi, 0)
 
-        # pxyzE calculation & construction : [px, py, pz, E]
-        Zlep1 = self.getKinVar(Zlep1var[0], Zlep1var[1], Zlep1var[2], 0.)
-        Zlep2 = self.getKinVar(Zlep2var[0], Zlep2var[1], Zlep2var[2], 0.)
-        Wlep = self.getKinVar(Wlepvar[0], Wlepvar[1], Wlepvar[2], 0.) ## Assume lepton mass = 0
-        bjet = self.getKinVar(bvar[0], bvar[1], bvar[2], bvar[3])
-        qjet = self.getKinVar(qvar[0], qvar[1], qvar[2], qvar[3])
+            # b jet assign by CSVv2 discriminator
+            if ( bjetCSV < qjetCSV ):
+                bvar, qvar = qvar, bvar
+                bjetCSV, qjetCSV = qjetCSV, bjetCSV
+
+            # pxyzE calculation & construction : [px, py, pz, E]
+            Zlep1 = self.getKinVar(Zlep1var.Pt(), Zlep1var.Eta(), Zlep1var.Phi(), 0.)
+            Zlep2 = self.getKinVar(Zlep2var.Pt(), Zlep2var.Eta(), Zlep2var.Phi(), 0.)
+            Wlep = self.getKinVar(Wlepvar.Pt(), Wlepvar.Eta(), Wlepvar.Phi(), 0.) ## Assume lepton mass = 0
+            bjet = self.getKinVar(bvar.Pt(), bvar.Eta(), bvar.Phi(), bvar.M())
+            qjet = self.getKinVar(qvar.Pt(), qvar.Eta(), qvar.Phi(), qvar.M())
+
+            # MET pxy calculation & construction : [px, py]
+            metpx = metvar.Pt()*(math.cos(metvar.Phi()))
+            metpy = metvar.Pt()*(math.sin(metvar.Phi()))
+            met = [metpx, metpy]
+
+            # Calculate pz_neu and E_neu
+            metpz = self.getSol(Wlep[0], Wlep[1], Wlep[2], Wlep[3], 0., met[0], met[1], Wmass) # Assume lepton mass = 0
+            posneuE = math.sqrt((metpx*metpx)+(metpy*metpy)+(metpz[0]*metpz[0]))
+            negneuE = math.sqrt((metpx*metpx)+(metpy*metpy)+(metpz[1]*metpz[1]))
+            consted = metpz[2] # Reconstructed flag
+
+            # SM Top mass construction
+            SMpxs = [bjet[0], Wlep[0], met[0]]
+            SMpys = [bjet[1], Wlep[1], met[1]]
+            posSMpzs = [bjet[2], Wlep[2], metpz[0]]
+            negSMpzs = [bjet[2], Wlep[2], metpz[1]]
+            posSMEs= [bjet[3], Wlep[3], posneuE]
+            negSMEs= [bjet[3], Wlep[3], negneuE]
+
+            posTMass = self.getTPEPM(SMpxs, SMpys, posSMpzs, posSMEs)[3]
+            negTMass = self.getTPEPM(SMpxs, SMpys, negSMpzs, negSMEs)[3]
         
-        # MET pxy calculation & construction : [px, py]
-        metpx = metvar[0]*(math.cos(metvar[1]))
-        metpy = metvar[0]*(math.sin(metvar[1]))
-        met = [metpx, metpy]
+            ## Top mass variance comparison and reconsruct SM Top KinVals
+           if ( math.fabs(posTMass - OriginTmass) < math.fabs(negTMass - OriginTmass) ):
+                SMTMass = posTMass
+                SMTKinVal = self.getTPEPM(SMpxs, SMpys, posSMpzs, posSMEs)
+            else:
+                SMTMass = negTMass
+                SMTKinVal = self.getTPEPM(SMpxs, SMpys, negSMpzs, negSMEs)
 
-        # Calculate pz_neu and E_neu
-        metpz = self.getSol(Wlep[0], Wlep[1], Wlep[2], Wlep[3], 0., met[0], met[1], Wmass) # Assume lepton mass = 0
-        posneuE = math.sqrt((metpx*metpx)+(metpy*metpy)+(metpz[0]*metpz[0]))
-        negneuE = math.sqrt((metpx*metpx)+(metpy*metpy)+(metpz[1]*metpz[1]))
-        consted = metpz[2] # Reconstructed flag
+            # FCNC Top reconstruction
+            FCNCpxs = [qjet[0], Zlep1[0], Zlep2[0]]
+            FCNCpys = [qjet[1], Zlep1[1], Zlep2[1]]
+            FCNCpzs = [qjet[2], Zlep1[2], Zlep2[2]]
+            FCNCEs = [qjet[3], Zlep1[3], Zlep2[3]]
+            FCNCTKinVal = self.getTPEPM(FCNCpxs, FCNCpys, FCNCpzs, FCNCEs)
 
-        # SM Top mass construction
-        SMpxs = [bjet[0], Wlep[0], met[0]]
-        SMpys = [bjet[1], Wlep[1], met[1]]
-        posSMpzs = [bjet[2], Wlep[2], metpz[0]]
-        negSMpzs = [bjet[2], Wlep[2], metpz[1]]
-        posSMEs= [bjet[3], Wlep[3], posneuE]
-        negSMEs= [bjet[3], Wlep[3], negneuE]
-
-        posTMass = self.getTPEPM(SMpxs, SMpys, posSMpzs, posSMEs)[3]
-        negTMass = self.getTPEPM(SMpxs, SMpys, negSMpzs, negSMEs)[3]
-        
-        ## Top mass variance comparison and reconsruct SM Top KinVals
-        if ( math.fabs(posTMass - OriginTmass) < math.fabs(negTMass - OriginTmass) ):
-            SMTMass = posTMass
-            SMTKinVal = self.getTPEPM(SMpxs, SMpys, posSMpzs, posSMEs)
-        else:
-            SMTMass = negTMass
-            SMTKinVal = self.getTPEPM(SMpxs, SMpys, negSMpzs, negSMEs)
-
-        # FCNC Top reconstruction
-        FCNCpxs = [qjet[0], Zlep1[0], Zlep2[0]]
-        FCNCpys = [qjet[1], Zlep1[1], Zlep2[1]]
-        FCNCpzs = [qjet[2], Zlep1[2], Zlep2[2]]
-        FCNCEs = [qjet[3], Zlep1[3], Zlep2[3]]
-        FCNCTKinVal = self.getTPEPM(FCNCpxs, FCNCpys, FCNCpzs, FCNCEs)
-
-        self.out.fillBranch("KinTop_status", consted)
-        self.out.fillBranch("KinTopWb_pt", SMTKinVal[0])
-        self.out.fillBranch("KinTopWb_eta", SMTKinVal[1])
-        self.out.fillBranch("KinTopWb_phi", SMTKinVal[2])
-        self.out.fillBranch("KinTopWb_mass", SMTKinVal[3])
-        self.out.fillBranch("KinTopZq_pt", FCNCTKinVal[0])
-        self.out.fillBranch("KinTopZq_eta", FCNCTKinVal[1])
-        self.out.fillBranch("KinTopZq_phi", FCNCTKinVal[2])
-        self.out.fillBranch("KinTopZq_mass", FCNCTKinVal[3])
+            self.out.fillBranch("KinTop_status", consted)
+            self.out.fillBranch("KinTopWb_pt", SMTKinVal[0])
+            self.out.fillBranch("KinTopWb_eta", SMTKinVal[1])
+            self.out.fillBranch("KinTopWb_phi", SMTKinVal[2])
+            self.out.fillBranch("KinTopWb_mass", SMTKinVal[3])
+            self.out.fillBranch("KinTopZq_pt", FCNCTKinVal[0])
+            self.out.fillBranch("KinTopZq_eta", FCNCTKinVal[1])
+            self.out.fillBranch("KinTopZq_phi", FCNCTKinVal[2])
+            self.out.fillBranch("KinTopZq_mass", FCNCTKinVal[3])
 
         ## for debugging
         #print " Coefficients A/B/aterm/bterm/cterm : ", metpz[3], metpz[4], metpz[5], metpz[6], metpz[7]
